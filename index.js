05b4d3c82f7d45b876dcc920d507070b66cc4eee
Jandrinupgrade <alejandro.suarez@bootcamp-upgrade.com>
2022-05-12 12:07:40 +0200

    ejerc 1,3,5

--- index.js
+++ index.js
@@ -10,4 +10,87 @@ let y = 10;
 //1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
 let z = h + y;
 console.log(z);
+//1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+const character = {name: 'Jack Sparrow', age: 10};
+character["age"] = 25;
+console.log(character["age"]);
+
+//1.2 Declara 3 variables con los nombres y valores siguientes 
+
+	firstName = 'Jon'; 
+	lastName = 'Snow'; 
+	age = 24; 
+//Muestralos por consola de esta forma: 
+//	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+console.log("soy",firstName,lastName+", tengo",age,"años y me gustan los lobos.");
+
+//1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
+//ambos juguetes.
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+console.log(toy1.price + toy2.price);
+//1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
+//y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
+//basePrice más el valor de la variable globalBasePrice.
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+globalBasePrice = 25000;
+car1.finalPrice = car1.finalPrice + globalBasePrice;
+car2.finalPrice = car2.finalPrice + globalBasePrice;
+console.log(car1.finalPrice);
+console.log(car2.finalPrice);
+
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+console.log(10*5);
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+console.log(10/2);
+
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+console.log(15%9);
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+//teniendo dos variables y = 10 y z = 5.
+y = 10;
+z = 5;
+x = y + z;
+console.log(x);
+//1.5 Usa el correcto operador de asignación que resultará en x = 50,
+//teniendo dos variables y = 10 y z = 5.
+x = y*z;
+console.log(x);
+
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+
+if(number1 === 10){
+    console.log('number1 es estrictamente igual a 10')
+}
+
+if (number2/number1 == 2) {
+  console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (number1 !== number2) {
+  console.log("number1 es estrictamente distinto a number2");
+}
+
+if (number3 != number1) {
+  console.log("number3 es distinto number1");
+}
+
+if (number3*5 == number1) {
+  console.log("number3 por 5 es igual a number1");
+}
+
+if ((number3*5 == number1) && (number1*2 ==number2)) {
+  console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+
+if ((number2/2==number1) || (number1/5 ==number3)) {
+  console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+}
+
 
