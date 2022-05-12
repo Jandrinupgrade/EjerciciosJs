2f3532b5ede55168c6bb71aa9a1c3c0e21ce4c36
Jandrinupgrade <alejandro.suarez@bootcamp-upgrade.com>
2022-05-12 14:48:03 +0200

    Ejercicios js completos

--- index.js
+++ index.js
@@ -10,36 +10,7 @@ let y = 10;
 //1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
 let z = h + y;
 console.log(z);
-//1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
-const character = {name: 'Jack Sparrow', age: 10};
-character["age"] = 25;
-console.log(character["age"]);
 
-//1.2 Declara 3 variables con los nombres y valores siguientes 
-
-	firstName = 'Jon'; 
-	lastName = 'Snow'; 
-	age = 24; 
-//Muestralos por consola de esta forma: 
-//	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
-console.log("soy",firstName,lastName+", tengo",age,"años y me gustan los lobos.");
-
-//1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
-//ambos juguetes.
-const toy1 = {name: 'Buss myYear', price: 19};
-const toy2 = {name: 'Rallo mcKing', price: 29};
-console.log(toy1.price + toy2.price);
-//1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
-//y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
-//basePrice más el valor de la variable globalBasePrice.
-let globalBasePrice = 10000;
-const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
-const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
-globalBasePrice = 25000;
-car1.finalPrice = car1.finalPrice + globalBasePrice;
-car2.finalPrice = car2.finalPrice + globalBasePrice;
-console.log(car1.finalPrice);
-console.log(car2.finalPrice);
 
 //1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
 console.log(10*5);
@@ -90,7 +61,67 @@ if ((number3*5 == number1) && (number1*2 ==number2)) {
 }
 
 if ((number2/2==number1) || (number1/5 ==number3)) {
-  console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+    
+    console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
 }
+
+// 1.1 Consigue el valor "HULK" del array de cars y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
+
+// 1.2 Cambia el primer elemento de avengers a "IRONMAN"
+// const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avengers[0]= "IROMAN";
+console.log(avengers);
+// 1.3 Alert numero de elementos en el array usando la propiedad correcta de Array.
+// const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers.length);
+//1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+//Muestra en consola el último personaje del array
+
+//  const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+//  rickAndMortyCharacters.push("Morty","Summer");
+//  console.log(rickAndMortyCharacters);
+// console.log(rickAndMortyCharacters[rickAndMortyCharacters.length - 1]);
+
+//1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+// rickAndMortyCharacters.pop();
+// console.log(rickAndMortyCharacters[0],rickAndMortyCharacters[rickAndMortyCharacters.length-1]);
+//1.6 Elimina el segundo elemento del array y muestra el array por consola.
+//const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+
+//1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+const character = {name: 'Jack Sparrow', age: 10};
+character.age = 25;
+console.log(character.age);
+
+//1.2 Declara 3 variables con los nombres y valores siguientes 
+
+	firstName = 'Jon'; 
+	lastName = 'Snow'; 
+	age = 24; 
+//Muestralos por consola de esta forma: 
+//	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+console.log("soy",firstName,lastName+", tengo",age,"años y me gustan los lobos.");
+
+//1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
+//ambos juguetes.
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+console.log(toy1.price + toy2.price);
+//1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
+//y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
+//basePrice más el valor de la variable globalBasePrice.
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+globalBasePrice = 25000;
+car1.finalPrice = car1.finalPrice + globalBasePrice;
+car2.finalPrice = car2.finalPrice + globalBasePrice;
+console.log(car1.finalPrice);
+console.log(car2.finalPrice);
 
 
