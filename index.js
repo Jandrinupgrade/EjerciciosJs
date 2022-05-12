4014bed07645429532b403cd2ec5cbda431ce1e0
Jandrinupgrade <alejandro.suarez@bootcamp-upgrade.com>
2022-05-12 17:04:30 +0200

    ejercicios completos

--- index.js
+++ index.js
@@ -86,12 +86,13 @@ console.log(avengers.length);
 
 //1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
 
-const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+//const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 // rickAndMortyCharacters.pop();
 // console.log(rickAndMortyCharacters[0],rickAndMortyCharacters[rickAndMortyCharacters.length-1]);
 //1.6 Elimina el segundo elemento del array y muestra el array por consola.
-//const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
-
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+ rickAndMortyCharacters.splice(1,1);
+console.log(rickAndMortyCharacters);
 
 //1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
 const character = {name: 'Jack Sparrow', age: 10};
@@ -123,5 +124,15 @@ car1.finalPrice = car1.basePrice + globalBasePrice;
 car2.finalPrice = car2.basePrice + globalBasePrice;
 console.log(car1.finalPrice);
 console.log(car2.finalPrice);
+
+// 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+// 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+// cuando el resto del numero dividido entre 2 sea 0.
+
+// 1.3 Crea un bucle para conseguir dormir contando ovejas. 
+// Este bucle empieza en 0 y termina en 10. 
+// Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle 
+// y cambia el mensaje en la última vuelta a 'Dormido!'.
 
 
